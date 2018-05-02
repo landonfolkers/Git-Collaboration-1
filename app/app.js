44096706ad808c867816cbd4a31f7de1d386080b
Pamela <PamelaYuhas@gmail.com>
2018-05-02 09:56:57 -0600

    created an array and function

--- app/app.js
+++ app/app.js
@@ -19,3 +19,13 @@ arr2 = [
         age: 41
     }
 ]
+
+arr3 = [19, 5, 90]
+
+function multiply (array) {
+    var sum = 1;
+    for (var i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum;
+}
